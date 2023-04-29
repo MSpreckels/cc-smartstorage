7c6cab711ed10921254658daa5ec93dd2301df11
Marco Spreckels <--->
2023-04-29 22:44:29 +0200

    gui

--- gui.lua
+++ gui.lua
@@ -42,7 +42,6 @@ end
 function GUI.run(self)
     local eventData = {os.pullEvent()}
     local event = eventData[1]
-    print(event)
     if event == "mouse_click" then
         print("mouse click at pos " .. eventData[3] .. "," .. eventData[4])
     elseif event == "key" then
